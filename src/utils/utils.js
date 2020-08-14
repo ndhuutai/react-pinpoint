419b93e450db24a5f5c55112083446a4248adaae
Tai N <ndhuutai1@gmail.com>
2020-08-14 16:07:48 -0700

    added Mattie wrapper functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,86 +1,91 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable consistent-return */
 
-class ReactPinpoint {
-  constructor() {
-    this.changes = [];
-  }
+let changes = [];
 
-  getCircularReplacer() {
-    const seen = new WeakSet();
-    return (key, value) => {
-      if (typeof value === 'object' && value !== null) {
-        if (seen.has(value)) {
-          return;
-        }
-        seen.add(value);
-      }
-      return value;
-    };
-  }
+function parseNode(node) {
+  return {
+    type: node.type,
+    selfBaseDuration: node.selfBaseDuration,
+    state: node.stateNode && node.stateNode.state && node.stateNode.state.next,
+    child: node.child && parseNode(node.child),
+    sibling: node.sibling && parseNode(node.sibling),
+  };
+}
 
-  recordChangesToObjField(obj, field) {
-    Object.defineProperty(obj, field, {
-      get() {
-        return this._current;
-      },
-      set(value) {
-        this.changes.push(JSON.stringify(value, this.getCircularReplacer()));
-        this._current = value;
-      },
-    });
-  }
+function recordChangesToObjField(obj, field) {
+  Object.defineProperty(obj, field, {
+    get() {
+      return this._current;
+    },
+    set(value) {
+      changes.push(parseNode(value));
+      console.log(value);
+      this._current = value;
+    },
+  });
+}
 
-  mountToReactRoot(reactRoot) {
-    // Reset changes
-    this.changes = [];
-    // Lift parent of react fibers tree
-    const parent = reactRoot._reactRootContainer._internalRoot;
-    const { current } = parent;
-    // Add listener to react fibers tree so changes can be recorded
-    this.recordChangesToObjField(parent, 'current');
-    // Reassign react fibers tree to record initial state
-    parent.current = current;
-  }
+function parseCompletedNode(node) {
+  return {
+    type: node.type.name,
+    state: node.state,
+    selfBaseDuration: node.selfBaseDuration,
+  };
+}
 
-  traverseWith(fiber, callback) {
-    callback(fiber);
-    if (fiber.child) {
-      this.traverseWith(fiber.child, callback);
-    }
-    if (fiber.sibling) {
-      this.traverseWith(fiber.sibling, callback);
-    }
-  }
+function mountToReactRoot(reactRoot) {
+  // Reset changes
+  changes = [];
 
-  flattenTree(tree) {
-    // Closured array for storing fibers
-    const arr = [];
-    // Closured callback for adding to arr
-    const callback = (fiber) => {
-      arr.push(fiber);
-    };
-    this.traverseWith(tree, callback);
-    return arr;
-  }
+  // Lift parent of react fibers tree
+  const parent = reactRoot._reactRootContainer._internalRoot;
+  const { current } = parent;
 
-  checkTime(fiber, threshold) {
-    return fiber.selfBaseDuration > threshold;
-  }
+  // Add listener to react fibers tree so changes can be recorded
+  recordChangesToObjField(parent, 'current');
 
-  /**
-   *
-   * @param {number} threshold The rendering time to filter for.
-   */
-  getAllSlowComponentRenders(threshold, changesArray) {
-    const slowRenders = changesArray
-      .map(JSON.parse) // Convert objects to JSON
-      .map(this.flattenTree) // Flatten tree
-      .flat() // Flatten 2d array into 1d array
-      .filter((fiber) => this.checkTime(fiber, threshold)); // filter out all that don't meet threshold
-    // Return any times greater than 16ms
-    return slowRenders;
+  // Reassign react fibers tree to record initial state
+  parent.current = current;
+  return changes;
+}
+
+function traverseWith(fiber, callback) {
+  callback(fiber);
+  if (fiber.child) {
+    traverseWith(fiber.child, callback);
+  }
+  if (fiber.sibling) {
+    traverseWith(fiber.sibling, callback);
   }
 }
 
-module.exports = { ReactPinpoint };
+function flattenTree(tree) {
+  // Closured array for storing fibers
+  const arr = [];
+  // Closured callback for adding to arr
+  const callback = (fiber) => {
+    arr.push(fiber);
+  };
+  traverseWith(tree, callback);
+  return arr;
+}
+
+function checkTime(fiber, threshold) {
+  return fiber.selfBaseDuration > threshold;
+}
+
+/**
+ *
+ * @param {number} threshold The rendering time to filter for.
+ */
+function getAllSlowComponentRenders(threshold) {
+  const slowRenders = changes
+    .map(flattenTree) // Flatten tree
+    .flat() // Flatten 2d array into 1d array
+    .filter((fiber) => checkTime(fiber, threshold)) // filter out all that don't meet threshold
+    .map(parseCompletedNode); // removes circular references
+  return slowRenders;
+}
+
+module.exports = { mountToReactRoot, getAllSlowComponentRenders, traverseWith };
