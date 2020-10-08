1deba87447b7849ef38e9179cdf875293c9ac793
Tai N <ndhuutai1@gmail.com>
2020-10-08 16:37:19 -0700

    added rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,53 @@
 // import path from "path";
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 const extensions = ['.ts', '.js'];
 
-export default {
-  input: './src/index.ts',
-  output: {
-    file: 'lib/bundle.es5.js',
-    format: 'esm',
+export default [
+  {
+    input: './src/utils/utils.ts',
+    output: {
+      file: 'lib/bundle.puppeteer.js',
+      format: 'esm',
+    },
+    plugins: [
+      resolve({
+        jsnext: true,
+        extensions,
+      }),
+      commonjs({
+        include: 'node_modules/**',
+      }),
+      babel({
+        extensions,
+        presets: ['@babel/preset-env', '@babel/preset-typescript'],
+        exclude: /node_modules/,
+      }),
+    ],
   },
-  plugins: [
-    resolve({
-      jsnext: true,
-      extensions,
-    }),
-    babel({
-      extensions,
-      presets: ['@babel/preset-env', '@babel/preset-typescript'],
-    }),
-  ],
-};
+  {
+    input: './src/index.ts',
+    output: {
+      file: 'lib/bundle.main.js',
+      format: 'esm',
+    },
+    plugins: [
+      resolve({
+        jsnext: true,
+        extensions,
+      }),
+      commonjs({
+        include: 'node_modules/**',
+      }),
+      babel({
+        extensions,
+        babelHelpers: 'runtime',
+        presets: ['@babel/preset-env', '@babel/preset-typescript'],
+        plugins: ['@babel/plugin-transform-runtime'],
+        exclude: /node_modules/,
+      }),
+    ],
+  },
+];
